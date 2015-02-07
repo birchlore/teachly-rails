04b5a08d46f56644afb8253de375c86b03a6b135
birchlore <jacksondcunningham@gmail.com>
2015-02-07 00:47:24 +0000

    add login page

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,25 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to users_path
+    else
+      flash.now[:alert] = "Login failed"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to new_session_url, notice: "You are now logged out."
+  end
+
+
+end
+
+
